13b0236037677ac6c3d759dab414c0028ce6aa94
Ibbo Abdoli <ibbo.abdoli@gmail.com>
2026-06-22 16:13:15 +0200

    fix: remove duplicate JavaScript app layout

--- app/layout.js
+++
@@ -1,5 +0,0 @@
-import React from "react";
-
-export default function RootLayout({ children }) {
-  return React.createElement("html", { lang: "en" }, React.createElement("body", null, children));
-}
